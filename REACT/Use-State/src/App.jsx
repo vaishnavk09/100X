353853c125dd0891fb7f24d05b712ff19c96e828
vaishnavk09 <vaishnavk9420@gmail.com>
2026-02-06 19:36:46 +0530

    component sizeGola

--- REACT/Use-State/src/App.jsx
+++ REACT/Use-State/src/App.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [size, setsize] = useState('100px')
+ 
+
+  return (
+   <SizeGola />
+
+  )
+}
+
+const SizeGola=()=>{
+   const [size, setsize] = useState('100px')
  
   const increase = () => {
     setsize(parseInt(size) + 100 + 'px')
   }
-
   return (
-    <div style={{ width: size, height: size, backgroundColor: 'lightblue', borderRadius: '50% 50% 50% 50%' }}>
+  <div style={{ width: size, height: size, backgroundColor: 'lightblue', borderRadius: '50% 50% 50% 50%' }}>
       <button onClick={increase}>make it Large</button>
     </div>
   )
 }
-
 export default App
